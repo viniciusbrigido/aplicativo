79fbfda50ff5c6740dd52ae4f8264b822358a6dd
vinicius.brigido <brigido.viniciuss@gmail.com>
2022-05-20 23:01:28 -0300

    Resolvido atividade para fazer o calculo do IMC

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,34 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  peso: number
+  altura: number
+  resultado: string
+  descricao: string
+
   constructor() {}
 
+  calcular() {
+    let imc: number = this.peso / (this.altura ** 2)
+    this.resultado = 'IMC: ' + imc.toFixed(2);
+    this.descricao = this.getDescricaoImc(imc);
+  }
+
+  getDescricaoImc(imc: number): string {
+    let descricaoImc: string = ''
+    if (imc < 18.5) {
+      descricaoImc = 'Abaixo do peso normal'
+    } else if (imc < 25) {
+      descricaoImc = 'Peso normal'
+    } else if (imc < 30) {
+      descricaoImc = 'Excesso de peso'
+    } else if (imc < 35) {
+      descricaoImc = 'Obesidade classe I'
+    } else if (imc < 40) {
+      descricaoImc = 'Obesidade classe II'
+    } else {
+      descricaoImc = 'Obesidade classe III'
+    }
+    return descricaoImc;
+  }
 }
