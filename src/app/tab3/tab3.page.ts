8def10d2f65a18dda494dc23839c5873f5d4a95e
vinicius.brigido <brigido.viniciuss@gmail.com>
2022-05-22 00:15:10 -0300

    Criado aba com dados pessoais

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -9,4 +9,19 @@ export class Tab3Page {
 
   constructor() {}
 
+  abreGitHub() {
+    window.open('https://github.com/viniciusbrigido', '_blank');
+  }
+
+  abreLinkedin() {
+    window.open('https://www.linkedin.com/in/vin%C3%ADcius-brigido-721673212/', '_blank');
+  }
+
+  abreInstagram() {
+    window.open('https://www.instagram.com/vinicius.brigido', '_blank');
+  }
+
+  abreTwitter() {
+    window.open('https://www.twitter.com/ViniciusBrigido', '_blank');
+  }
 }
