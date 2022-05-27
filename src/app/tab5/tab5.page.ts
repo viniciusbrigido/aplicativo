7f3b76aaa032a0296bf638740181e82ed6af900f
vinicius.brigido <brigido.viniciuss@gmail.com>
2022-05-26 23:02:08 -0300

    Criado calculo de fatorial e raiz quadrada e criado validacoes para as operacoes

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -15,38 +15,78 @@ export class Tab5Page {
   constructor(private toastController: ToastController) { }
 
   somar() {
+    if (this.num1 == null || this.num2 == null) {
+      this.exibirToast('Preencha os dois números.');
+      return;
+    }
     this.resultado = this.num1 + this.num2;
   }
 
   subtrair() {
+    if (this.num1 == null || this.num2 == null) {
+      this.exibirToast('Preencha os dois números.');
+      return;
+    }
     this.resultado = this.num1 - this.num2;
   }
 
   multiplicar() {
+    if (this.num1 == null || this.num2 == null) {
+      this.exibirToast('Preencha os dois números.');
+      return;
+    }
     this.resultado = this.num1 * this.num2;
   }
 
   dividir() {
+    if (this.num1 == null || this.num2 == null) {
+      this.exibirToast('Preencha os dois números.');
+      return;
+    }
     if (this.num1 === 0 || this.num2 === 0) {
-      this.exibirToast('Divisão por zero.');
+      this.exibirToast('Divisão por zero não permitida.');
       return;
     }
     this.resultado = this.num1 / this.num2;
   }
 
   raiz() {
-    this.resultado = this.num1 / this.num2;
+    if (this.num1 == null) {
+      this.exibirToast('Preencha o número 1.');
+      return;
+    }
+    this.resultado = Math.sqrt(this.num1);
   }
 
   fatorial() {
-    this.resultado = this.num1 / this.num2;
+    if (this.num1 == null) {
+      this.exibirToast('Preencha o número 1.');
+      return;
+    }
+    this.resultado = this.fatorialResumido(this.num1);
+  }
+
+  fatorialResumido(value: number): number {
+    if (value === 0) {
+      return 1;
+    } else {
+      return value * this.fatorialResumido(value - 1);
+    }
+  }
+
+  fatorialExtendido(value: number): number {
+    let fatorial = 1;
+    for (let i = 1; i <= value; i++) {
+      fatorial = fatorial * i;
+    }
+    return fatorial;
   }
 
   async exibirToast(msg: string) {
     const toast = await this.toastController.create({
       message: msg,
       duration: 2000,
-      position: 'bottom',
+      position: 'top',
       icon: 'hand-left-outline'
     });
     toast.present();
