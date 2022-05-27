ede358477938f560de698ff871d6d16e6e06ca91
aluno <aluno@tub-depe-a1334.tubarao.ifsc.edu.br>
2022-05-26 21:34:30 -0300

    Criado aba de calculadora

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab5',
   templateUrl: './tab5.page.html',
   styleUrls: ['./tab5.page.scss'],
 })
-export class Tab5Page implements OnInit {
+export class Tab5Page {
 
-  constructor() { }
+  num1: number;
+  num2: number;
+  resultado: number;
 
-  ngOnInit() {
+  constructor(private toastController: ToastController) { }
+
+  somar() {
+    this.resultado = this.num1 + this.num2;
   }
 
+  subtrair() {
+    this.resultado = this.num1 - this.num2;
+  }
+
+  multiplicar() {
+    this.resultado = this.num1 * this.num2;
+  }
+
+  dividir() {
+    if (this.num1 === 0 || this.num2 === 0) {
+      this.exibirToast('Divisão por zero.');
+      return;
+    }
+    this.resultado = this.num1 / this.num2;
+  }
+
+  raiz() {
+    this.resultado = this.num1 / this.num2;
+  }
+
+  fatorial() {
+    this.resultado = this.num1 / this.num2;
+  }
+
+  async exibirToast(msg: string) {
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000,
+      position: 'bottom',
+      icon: 'hand-left-outline'
+    });
+    toast.present();
+  }
 }
