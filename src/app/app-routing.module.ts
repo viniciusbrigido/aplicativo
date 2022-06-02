354f48949b31d144248a5558a8d54b365b996005
aluno <aluno@tub-depe-a1321.tubarao.ifsc.edu.br>
2022-06-02 18:50:38 -0300

    Retirado pasta especifica do android e retirado aba sobre o mapa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,25 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./map/map.module').then( m => m.MapPageModule)
-  },
-  {
-    path: 'tab4',
-    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
-  },
-  {
-    path: 'tab5',
-    loadChildren: () => import('./tab5/tab5.module').then( m => m.Tab5PageModule)
-  },
-  {
-    path: 'tab6',
-    loadChildren: () => import('./tab6/tab6.module').then( m => m.Tab6PageModule)
-  },
-  {
-    path: 'map',
-    loadChildren: () => import('./map/map.module').then( m => m.MapPageModule)
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
   }
-
 ];
 @NgModule({
   imports: [
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -11,22 +11,22 @@ const routes: Routes = [
         path: 'tab1',
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
       },
-      // {
-      //   path: 'tab2',
-      //   loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
-      // },
-      // {
-      //   path: 'tab3',
-      //   loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-      // },
-      // {
-      //   path: 'tab4',
-      //   loadChildren: () => import('../tab4/tab4.module').then(m => m.Tab4PageModule)
-      // },jjjj
-      // {
-      //   path: 'tab5',
-      //   loadChildren: () => import('../tab5/tab5.module').then(m => m.Tab5PageModule)
-      // },
+      {
+        path: 'tab2',
+        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+      },
+      {
+        path: 'tab3',
+        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+      },
+      {
+        path: 'tab4',
+        loadChildren: () => import('../tab4/tab4.module').then(m => m.Tab4PageModule)
+      },
+      {
+        path: 'tab5',
+        loadChildren: () => import('../tab5/tab5.module').then(m => m.Tab5PageModule)
+      },
       {
         path: 'tab6',
         loadChildren: () => import('../tab6/tab6.module').then(m => m.Tab6PageModule)
