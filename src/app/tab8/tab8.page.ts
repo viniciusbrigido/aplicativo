e4b5ba19b8ca7cb0de23917491081ba7728250af
aluno <aluno@tub-depe-a1321.tubarao.ifsc.edu.br>
2022-06-02 21:41:40 -0300

    Adicionado capacitor par gerar build em android

--- src/app/tab8/tab8.page.ts
+++ src/app/tab8/tab8.page.ts
@@ -1,15 +1,16 @@
 import { ToastController } from '@ionic/angular';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-tab7',
-  templateUrl: './tab7.page.html',
-  styleUrls: ['./tab7.page.scss'],
+  selector: 'app-tab8',
+  templateUrl: './tab8.page.html',
+  styleUrls: ['./tab8.page.scss'],
 })
-export class Tab7Page {
+export class Tab8Page {
 
-  numeroSecreto: number;
-  numeroAdvinhacao: number;
+  palavras: string;
+  palavraSecreta: string;
+  palavraAdvinhacao: string;
   tentativasRestantes: number;
   mensagem: string;
   desabilita: boolean;
@@ -19,25 +20,23 @@ export class Tab7Page {
   }
 
   iniciaAdvinhacao() {
-    this.numeroSecreto = Math.floor(Math.random() * 10 + 1);
-    this.numeroAdvinhacao = null;
+    const palavras = ['amarelo', 'azul', 'verde', 'vermelho', 'rosa', 'preto'];
+    this.palavras = palavras.join(', ');
+    this.palavraSecreta = palavras[Math.floor(Math.random() * palavras.length)];
+    this.palavraAdvinhacao = null;
     this.tentativasRestantes = 8;
     this.mensagem = '';
     this.desabilita = false;
-    console.log(this.numeroSecreto);
+    console.log(this.palavraSecreta);
   }
 
   tentarAdvinhar() {
-    if (this.numeroSecreto === this.numeroAdvinhacao) {
-      this.exibirMensagem('Você acertou o número secreto. Deseja jogar novamente?');
+    if (this.palavraSecreta === this.palavraAdvinhacao) {
+      this.exibirMensagem('Você acertou a palavra secreta. Deseja jogar novamente?');
       return;
     }
-    if (this.numeroSecreto > this.numeroAdvinhacao) {
-      this.mensagem = 'O número secreto é maior';
-    } else {
-      this.mensagem = 'O número secreto é menor';
-    }
     this.tentativasRestantes--;
+    this.mensagem = 'Palavra errada';
 
     if (this.tentativasRestantes === 0)  {
       this.exibirMensagem('Número de Tentativas Esgotadas. Deseja jogar novamente?');
@@ -69,4 +68,5 @@ export class Tab7Page {
     });
     toast.present();
   }
+
 }
